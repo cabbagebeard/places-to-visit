46b935db1d2e54b58b4e28081cc58af3c4d442b4
Kevin Windisch <kwindisch1992@gmail.com>
2017-07-07 19:46:54 +0000

    code styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,42 +5,42 @@ class PostsController < ApplicationController
   end
   
   def create
-      @post = Post.new(post_params)
-      if @post.save
-          redirect_to @post
-      else
-          render 'new'
-      end
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
     
   def show
-      @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
   
   def edit
-      @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
   
   def update
-      @post = Post.find(params[:id])
-      if @post.update(params[:post].permit(:title, :description, :image_url))
-          redirect_to @post
-      else
-          render 'edit'
-      end
+    @post = Post.find(params[:id])
+    if @post.update(params[:post].permit(:title, :description, :image_url))
+        redirect_to @post
+    else
+        render 'edit'
+    end
   end
   
   def destroy
-      @post = Post.find(params[:id])
-      @post.destroy
-      
-      redirect_to root_path
+    @post = Post.find(params[:id])
+    @post.destroy
+    
+    redirect_to root_path
   end
   
   private
   
   def post_params
-      params.require(:post).permit(:title, :description, :image_url)
+    params.require(:post).permit(:title, :description, :image_url)
   end
   
 end
