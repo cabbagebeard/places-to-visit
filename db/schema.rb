5ef0b094269d13e85c83ee086075ba20bb9edf84
Kevin Windisch <kwindisch1992@gmail.com>
2017-07-08 19:11:30 +0000

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170707191837) do
+ActiveRecord::Schema.define(version: 20170708190242) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20170707191837) do
     t.text     "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "country"
+    t.string   "continent"
   end
 
 end
